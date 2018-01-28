d17e88c648722127c378dbecf0896e7befd19574
khuong3110 <davidkhuong3110@gmail.com>
2018-01-28 18:45:16 -0500

    add files

---
+++ Debug-Exercise-master/classes/fruit_product.class.php
@@ -0,0 +1,36 @@
+<?php
+//missed include file from product.class.php
+include_once(ABSOLUTE_PATH . '/webconfig.php');
+include_once(ABSOLUTE_PATH . '/classes/product.class.php');
+
+
+class fruit extends product {
+
+	protected $product_type = "f";
+
+	function __construct($product_id, $product_name, $price_type, $product_price)
+		{
+			$this->product_id = $product_id;
+			$this->product_name = $product_name;
+			$this->price_type = $price_type;
+			$this->product_price = $product_price;
+		}
+
+	function __get($variablename)
+		{
+			return $this->$variablename;
+		}
+
+	function __set($variablename, $variablevalue)
+		{
+			$this->$variablename = $variablevalue;
+		}
+
+	function __destruct()
+		{
+		}
+
+
+}
+
+?>
---
+++ Debug-Exercise-master/webconfig.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+	//updated ABSOLUTE_PATH, URL_ROOT
+define('ABSOLUTE_PATH', '/home/kkhuat/htdocs/a1/Debug-Exercise-master');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:21681/a1/Debug-Exercise-master');
+?>
